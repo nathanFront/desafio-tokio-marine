d147efba2d3b27dc10ff6e17ed300c71281e1f2a
nathanFront <borges10nathan@gmail.com>
2022-06-23 17:06:22 -0300

    firts commit

---
+++ src/main/java/com/example/api/domain/Error.java
@@ -0,0 +1,10 @@
+package com.example.api.domain;
+
+public class Error {
+
+    String erro;
+
+    public Error(String erro) {
+        this.erro = erro;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+SELECT * FROM address;
+
+SELECT * FROM customer;
